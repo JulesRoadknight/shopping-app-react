7006fee339263251baabf0c2e3654803d654e8ca
Jimjule <roadknightj@gmail.com>
2020-10-02 17:00:22 +0100

    Includes requestLib file for previous commit

---
+++ src/libs/requestLib.js
@@ -0,0 +1,20 @@
+  export async function makeRequest(method, address, details = null) {
+    const config = method === 'GET' ?
+      {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        }
+      }
+      :
+      {
+        method: method,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(details)
+      }
+    await fetch(`http://localhost:4000/${address}`, config)
+  }
